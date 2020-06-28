3eff37557dcbb118979a96ef1d2c88b0a77f94f7
LinchSmyth <andrei95-1995@mail.ru>
2020-06-28 12:27:23 +0300

    (feat) make PresenceValidation work

--- lib/validator/presence_validation.rb
+++ lib/validator/presence_validation.rb
@@ -3,7 +3,11 @@ require_relative 'value_validation'
 module Validator
   class PresenceValidation < ValueValidation
   
-  
+    def validate_attribute(object, attribute, value)
+      if value.nil? == option
+        raise_validation_error("`#{ attribute }` should be #{ option ? 'present' : 'nil' }")
+      end
+    end
   
   end
 end
