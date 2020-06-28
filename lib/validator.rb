39ab78f09ea7a39b9e8ac11e07b7041400deef8d
LinchSmyth <andrei95-1995@mail.ru>
2020-06-28 11:48:46 +0300

    (feat) add `::validate` method with stubs to other methods

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,6 +1,40 @@
-require "validator/version"
+require 'validator/version'
 
 module Validator
-  class Error < StandardError; end
-  # Your code goes here...
+  ValidationError = Class.new(StandardError)
+  
+  
+  def valid?
+    raise NotImplementedError
+  end
+  
+  def validate!
+    raise NotImplementedError
+  end
+  
+
+  def self.included(base)
+    base.instance_variable_set(:@validations, Hash.new { |hh, key| hh[key] = [] })
+    base.extend(ClassMethods)
+  end
+  
+  module ClassMethods
+    attr_accessor :validations
+    
+    def validate(*fields, **options)
+      fields.each(field) do |field|
+        validations[field.to_sym] << parse_options(options)
+      end
+    end
+    
+    
+    private
+    
+    
+    def parse_options(options)
+      raise NotImplementedError
+    end
+    
+  end
+  
 end
