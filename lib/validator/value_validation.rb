85b1a19bc57bf7189db8242d89e30aa66a1cee8c
LinchSmyth <andrei95-1995@mail.ru>
2020-06-28 12:27:12 +0300

    (feat) make Value::ValueValidation work

--- lib/validator/value_validation.rb
+++ lib/validator/value_validation.rb
@@ -1,6 +1,24 @@
 module Validator
   class ValueValidation
   
+    attr_reader :option
+  
+    def initialize(option)
+      @option = option
+    end
+  
+    def perform_validation(object, attribute)
+      value = object.public_send(attribute)
+      validate_attribute(object, attribute, value)
+    end
+  
+    def validate_attribute(object, attribute, value)
+      raise NotImplementedError, 'descendants must define #validate_attribute method'
+    end
+    
+    def raise_validation_error(msg)
+      raise ValidationError, msg
+    end
     
   end
 end
